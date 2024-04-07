a4a1558c1ba11810b29884a9451c46a7c7011738
izetter <59151193+izetter@users.noreply.github.com>
2024-04-06 21:04:41 -0600

    Add AWS Elastic IP and associate it with EC2 instance

--- EC2.tf
+++ EC2.tf
@@ -1,10 +1,20 @@
-resource "aws_instance" "my_ec2" {
+resource "aws_instance" "my_eip_instance" {
 	ami           = var.ami_id
 	instance_type = var.instance_type
 	subnet_id     = aws_subnet.my_public_subnet.id
-	# key_name      = var.key_name
-
+	vpc_security_group_ids = [aws_security_group.sandbox_security_group.id]
 	tags = {
 		Name = var.name_tag
 	}
+}
+
+resource "aws_eip" "my_eip" {
+	domain   = "vpc"
+	instance = aws_instance.my_eip_instance.id
+	depends_on = [aws_internet_gateway.my_igw]
+}
+
+resource "aws_eip_association" "my_eip_ec2_association" {
+  instance_id   = aws_instance.my_eip_instance.id
+  allocation_id = aws_eip.my_eip.id
 }
