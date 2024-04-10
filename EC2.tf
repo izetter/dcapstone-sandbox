1ee61f1bed7d692c080454fa769d55cf6583448d
izetter <59151193+izetter@users.noreply.github.com>
2024-04-09 21:44:54 -0600

    Commented out AWS Elastic IP and its EC2 association

--- EC2.tf
+++ EC2.tf
@@ -10,13 +10,13 @@ resource "aws_instance" "my_eip_instance" {
 	}
 }
 
-resource "aws_eip" "my_eip" {
-	domain   = "vpc"
-	instance = aws_instance.my_eip_instance.id
-	depends_on = [aws_internet_gateway.my_igw]
-}
+# resource "aws_eip" "my_eip" {
+# 	domain   = "vpc"
+# 	instance = aws_instance.my_eip_instance.id
+# 	depends_on = [aws_internet_gateway.my_igw]
+# }
 
-resource "aws_eip_association" "my_eip_ec2_association" {
-  instance_id   = aws_instance.my_eip_instance.id
-  allocation_id = aws_eip.my_eip.id
-}
+# resource "aws_eip_association" "my_eip_ec2_association" {
+# 	instance_id   = aws_instance.my_eip_instance.id
+# 	allocation_id = aws_eip.my_eip.id
+# }
